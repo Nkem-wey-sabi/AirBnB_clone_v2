874660e66325c0809daecbceea7bf9ee459dc975
root <ayesiganobert@gmail.com>
2021-12-21 17:48:13 +0000

    place edited

--- models/place.py
+++ models/place.py
@@ -10,8 +10,7 @@ from sqlalchemy.sql.schema import Table
 from sqlalchemy.orm import relationship
 
 
-if storage_type == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
+place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id'),
                                  primary_key=True,
