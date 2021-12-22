544f8e95a2168f016bf83162aa6b897041d35e97
Emediong Francis <emediongfrancis@gmail.com>
2021-12-22 01:01:29 +0100

    Fix PEP8

--- models/place.py
+++ models/place.py
@@ -11,15 +11,14 @@ from sqlalchemy.orm import relationship
 
 
 place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False)
-                          )
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True,
+                             nullable=False))
 
 
 class Place(BaseModel, Base):
